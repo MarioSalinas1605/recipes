054d44ad68a77c072fc16699d32e007553ac9dca
Mario Salinas <mario_1605@live.com.mx>
2020-09-18 17:07:58 -0500

    feat: Home with recipes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
+import { Link } from "react-router-dom"
+
+import mealdb from '../mealdb-api'
 
 export const Home = () => {
+    const [recipes, setRecipes] = useState(null)
+    const [isLoading, setisLoading] = useState(true)
 
-    return <h1>Home</h1>
+    const fetchMeal = async () => {
+        try {
+            const data = await mealdb.getLatest()
+            console.log(data)
+            setRecipes(data)
+            setisLoading(false)
+        } catch (error) {
+            console.log(error)
+            setRecipes(null)
+        }
+    }
+
+    useEffect(() => {
+        fetchMeal()
+    }, [])
+
+    if (isLoading) {
+        return <div className="message">Cargando...</div>
+    }
+
+    return <>
+        <Helmet>
+            <title>Recetas</title>
+        </Helmet>
+
+        <div className="recipes">
+            {recipes && recipes.map((recipe) => (
+                <Link to={`/recipe/${recipe.id}`} className="recipe" key={recipe.id}>
+                    <span className="bg" style={{ backgroundImage: `url(${recipe.thumbnail})` }}></span>
+                    <span className="info">
+                        <h2>{recipe.name}</h2>
+                    </span>
+                </Link>
+            ))}
+        </div>
+    </>
 }
